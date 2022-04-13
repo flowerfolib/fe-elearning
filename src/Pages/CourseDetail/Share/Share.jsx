71d274033777e2c4cbf86c30bb04fb2c2819d062
tans <trunganhcoder@gmail.com>
2022-04-13 17:17:28 +0700

    update course detail

--- src/Pages/CourseDetail/Share/Share.jsx
+++ src/Pages/CourseDetail/Share/Share.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import styles from './Share.module.css'
+import React from "react";
+import styles from "./Share.module.css";
+import {
+  FacebookMessengerIcon,
+  FacebookIcon,
+  FacebookShareButton,
+  FacebookMessengerShareButton,
+} from "react-share";
 
 function Share() {
+  const currentLocation = window.location.href;
+  console.log(currentLocation);
   return (
-    <div>Share</div>
-  )
+    <div>
+      <FacebookShareButton
+        url={currentLocation}
+        quote={"Đào tạo Diligo"}
+        hashtag={"#daotao_diligo"}
+      >
+        <FacebookIcon size={48} round /> Chia sẻ lên Facbook
+      </FacebookShareButton>
+      <FacebookMessengerShareButton url={currentLocation}>
+        <FacebookMessengerIcon size={48} round /> Chia sẻ lên Messenger
+      </FacebookMessengerShareButton>
+    </div>
+  );
 }
 
-export default Share
+export default Share;
