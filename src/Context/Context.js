555081ff91abe6f86e9b6a9f7af0ddd7ffedea9d
tans <trunganhcoder@gmail.com>
2022-04-19 11:32:00 +0700

    update api

--- src/Context/Context.js
+++ src/Context/Context.js
@@ -14,7 +14,7 @@ export const DataProvider = ({ children }) => {
     const [current, setCurrent] = useState()
     const [currentList, setCurrentList] = useState([])
     useEffect(() => {
-        fetch('http://192.168.1.121:15000/api/v1/slide_channel')
+        fetch('http://hrm.diligo.vn/api/v1/slide_channel')
             .then(result => result.json())
             .then(data => {
                 setData(data.data)
